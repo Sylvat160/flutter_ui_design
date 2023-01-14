5f75dc5d9095c8f3009f1a0fae701ef92f9151f3
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-14 17:28:04 +0000

    installing depencies

---
+++ lib/screens/onboarding/onboarding_screens.dart
@@ -0,0 +1,33 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          RiveAnimation.asset("assets/RiveAssets/shapes.riv"),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: 20,
+                sigmaY: 10,
+              ),
+              child: SizedBox(),
+            )
+          ),
+        ],
+      ),
+    );
+  }
+}
