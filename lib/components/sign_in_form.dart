f80d82790b23c3372f4579c1552805e51c347fa5
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-27 16:31:13 +0000

    animation done

--- lib/components/sign_in_form.dart
+++ lib/components/sign_in_form.dart
@@ -1,84 +1,214 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:rive/rive.dart';
 
-
-class SignInForm extends StatelessWidget {
+class SignInForm extends StatefulWidget {
   const SignInForm({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<SignInForm> createState() => _SignInFormState();
+}
+
+class _SignInFormState extends State<SignInForm> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool _isShowLoading = false;
+  bool _isShowConfetti = false;
+
+  late SMITrigger check;
+  late SMITrigger error;
+  late SMITrigger reset;
+
+  late SMITrigger confetti;
+
+  StateMachineController getRiverController(Artboard artboard) {
+    StateMachineController? controller =
+        StateMachineController.fromArtboard(artboard, "State Machine 1");
+    artboard.addController(controller!);
+    return controller;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Form(
-        child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Stack(
       children: [
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Text(
-            'Email Address',
-            style: TextStyle(
-              color: Colors.black54,
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: TextFormField(
-            decoration: InputDecoration(
-              prefixIcon: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: SvgPicture.asset("assets/icons/email.svg"),
-              ),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Text(
-            'Password',
-            style: TextStyle(
-              color: Colors.black54,
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: TextFormField(
-            obscureText: true,
-            decoration: InputDecoration(
-              prefixIcon: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: SvgPicture.asset("assets/icons/password.svg"),
-              ),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: ElevatedButton.icon(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0XFFF77D8E),
-              minimumSize: Size(double.infinity, 56),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(25),
-                  bottomRight: Radius.circular(25),
-                  bottomLeft: Radius.circular(25),
+        Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  'Email Address',
+                  style: TextStyle(
+                    color: Colors.black54,
+                  ),
                 ),
               ),
-            ),
-            icon: Icon(
-              CupertinoIcons.arrow_right,
-              color: Color(0XFFFE0037),
-            ),
-            label: Text("Sign In"),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please enter your email address";
+                    }
+                    return null;
+                  },
+                  onSaved: (email) {},
+                  decoration: InputDecoration(
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: SvgPicture.asset("assets/icons/email.svg"),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  'Password',
+                  style: TextStyle(
+                    color: Colors.black54,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please enter your password";
+                    }
+                    return null;
+                  },
+                  onSaved: (password) {
+                    print(password);
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: SvgPicture.asset("assets/icons/password.svg"),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    // Refactoring code
+                    
+                    // before, loading icon
+                    setState(() {
+                      _isShowLoading = true;
+                    });
+                    Future.delayed(Duration(seconds: 1), () {
+                      // if form is valid
+                      if (_formKey.currentState!.validate()) {
+                        // _formKey.currentState!.save();
+                        check.fire();
+                        Future.delayed(Duration(seconds: 2), () {
+                          setState(() {
+                            _isShowLoading = false;
+                            _isShowConfetti = true;
+                          });
+                          confetti.fire();
+                        });
+                      }
+                      //else
+                      else {
+                        error.fire();
+                        Future.delayed(Duration(seconds: 2), () {
+                          setState(() {
+                            _isShowLoading = false;
+                          });
+                        });
+                      }
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0XFFF77D8E),
+                    minimumSize: Size(double.infinity, 56),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(25),
+                        bottomRight: Radius.circular(25),
+                        bottomLeft: Radius.circular(25),
+                      ),
+                    ),
+                  ),
+                  icon: Icon(
+                    CupertinoIcons.arrow_right,
+                    color: Color(0XFFFE0037),
+                  ),
+                  label: Text("Sign In"),
+                ),
+              ),
+            ],
           ),
         ),
+        _isShowLoading
+            ? CustomPositioned(
+                child: RiveAnimation.asset(
+                  'assets/RiveAssets/check.riv',
+                  onInit: (artboard) {
+                    StateMachineController controller =
+                        getRiverController(artboard);
+                    check = controller.findSMI("Check") as SMITrigger;
+                    error = controller.findSMI("Error") as SMITrigger;
+                    reset = controller.findSMI("Reset") as SMITrigger;
+                  },
+                ),
+              )
+            : const SizedBox(),
+        _isShowConfetti
+            ? CustomPositioned(
+                child: Transform.scale(
+                scale: 6,
+                child: RiveAnimation.asset(
+                  "assets/RiveAssets/confetti.riv",
+                  onInit: (arboard) {
+                    StateMachineController controller =
+                        getRiverController(arboard);
+
+                    confetti =
+                        controller.findSMI("Trigger explosion") as SMITrigger;
+                  },
+                ),
+              ))
+            : const SizedBox(),
       ],
-    ));
+    );
+  }
+}
+
+class CustomPositioned extends StatelessWidget {
+  const CustomPositioned({super.key, required this.child, this.size = 100});
+  final Widget child;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: Column(
+        children: [
+          Spacer(),
+          SizedBox(
+            width: size,
+            height: size,
+            child: child,
+          ),
+          const Spacer(
+            flex: 1,
+          ),
+        ],
+      ),
+    );
   }
 }
